3f779b8181e3b0860d040120ae644bcb5e726acf
MaciekWin3 <maciekwin3@gmail.com>
2020-05-12 14:42:44 +0200

    Login screen and Firebase setup

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:mymusichub/screens/authenticate/authenticate.dart';
+
+import 'home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //home or authentiacte widget
+    return Authenticate();
+  }
+}
