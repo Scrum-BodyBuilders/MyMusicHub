bf7338015b11ad7a453e746bf63572521b84aa71
mateuszsobczyk <sobczyk.m@outlook.com>
2020-05-31 23:19:56 +0200

    initial homescreen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:mymusichub/screens/authenticate/authenticate.dart';
 import 'package:mymusichub/services/auth.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class Home extends StatelessWidget {
-
   final AuthService _auth = AuthService();
 
+  var projectName = 'My Project 1';
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Scaffold(
-        backgroundColor: Colors.brown[50],
-        appBar: AppBar(
-          title: Text('MyMusicHub'),
-          backgroundColor: Colors.brown[400],
-          elevation: 0.0,
-          actions: <Widget>[
-            FlatButton.icon(
-              icon: Icon(Icons.person),
-              label: Text('Logout'),
-              onPressed: () async {
-                await _auth.signOut();
-              },
-            ),
-          ],
-        ),
+        child: Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        title: Text(projectName),
       ),
-    );
+      body: SlidingUpPanel(
+        minHeight: 50,
+        maxHeight: 600,
+        collapsed: Container(
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
+
+          ),
+        ),
+        panel: Text('text'),
+      ),
+    ));
   }
 }
